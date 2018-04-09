83c85887fe23a6bb2fcd3cac0f9e9c56616428ce
baileyrobinson <37555330+baileyrobinson@users.noreply.github.com>
2018-04-09 15:54:13 -0400

    blah
    blah

--- Assets/Game/Classes/movement.cs
+++ Assets/Game/Classes/movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public GameObject player;
     public float speed = 25;
     public int startingStamina = 100;                            // The amount of stamina the player starts the game with.
     public float currentStamina;                                   // The current stamina the player has.
@@ -19,11 +20,12 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftShift) && currentStamina > 0)
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && currentStamina > 0)
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed * 2);
 
@@ -36,28 +38,31 @@ public class movement : MonoBehaviour
                 transform.Translate(Vector3.forward * Time.deltaTime * speed);
             }
         }
-
-        if (currentStamina < startingStamina)
+        else
         {
-            currentStamina += Time.deltaTime;
+            if (currentStamina < startingStamina)
+            {
+                currentStamina += Time.deltaTime;
+            }
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * Time.deltaTime * speed);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0, -5f, 0);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 5f, 0);
         }
     }
 }
+
 
 
 
