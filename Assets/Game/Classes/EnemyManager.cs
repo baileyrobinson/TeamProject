afb5aea7c03caf2bf763645e09205194bc5bea9a
Kevmonk2000 <37087819+Kevmonk2000@users.noreply.github.com>
2018-04-09 16:51:29 -0400

    Spawning
    yes

---
+++ Assets/Game/Classes/EnemyManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class wave {
+    public int EnemiesPerWave;
+    public GameObject Enemy;
+}
+public class EnemyManager : MonoBehaviour {
+
+    public wave[] waves;
+    public Transform[] SpawnPoints;
+    public float SpawnTime = 2f;
+
+    private int totalEnemiesInWave;
+    private int enemiesLeft;
+    private int spawnedEnemies;
+
+    private int currentWave;
+    private int totalWaves;
+
+	// Use this for initialization
+	void Start () {
+
+        currentWave = -1;
+        totalWaves = waves.Length - 1;
+
+        StartNextWave();
+
+	}
+
+    void StartNextWave()
+    {
+        currentWave++;
+
+        //end game or win
+        if (currentWave > totalWaves)
+        {
+            return;
+        }
+
+        totalEnemiesInWave = waves[currentWave].EnemiesPerWave;
+        enemiesLeft = 0;
+        spawnedEnemies = 0;
+
+        //StartCoroutine(SpawnEnemies());
+    }
+    /*
+    IEnumerator SpawnEnemies()
+    {
+
+    }
+    */
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
