81e173521110efe9504fb549a333815c4adc8247
KyleWelch00 <37556482+KyleWelch00@users.noreply.github.com>
2018-04-11 16:10:45 -0400

    Added Weapon-related scripts
    New scripts: Weapon Upgrade, Player Inventory, PlayerWeapon1
    
    Player Inventory tracks materials, money, and weapon level.
    
    Weapon Upgrade accesses those variables to level up the player's weapon
    
    PlayerWeapon1 controls how much damage the player deals.

--- Assets/Game/Classes/EnemyHealth.cs
+++ Assets/Game/Classes/EnemyHealth.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour {
 
     public int startingHealth = 100;                            // The amount of health the player starts the game with.
-    public int currentHealth;
+    static public int currentHealth;
 
     // Use this for initialization
     void Start () {
