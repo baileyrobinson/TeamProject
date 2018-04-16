127d9119c5fc3476f5b32868264dce4f8fdad139
KyleWelch00 <37556482+KyleWelch00@users.noreply.github.com>
2018-04-16 16:36:08 -0400

    Moved damage responsibility to Enemy
    Removed DealDamage from PlayerWeapon1, added TakeDamage to EnemyHealth. Began implementing an OnTriggerEnter for PlayerWeapon1 to trigger EnemyHelath's TakeDamage.

--- Assets/Game/Classes/Enemy/EnemyHealth.cs
+++ Assets/Game/Classes/Enemy/EnemyHealth.cs
@@ -9,11 +9,20 @@ public class EnemyHealth : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+        currentHealth = startingHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void TakeDamage(int value)
+    {
+        currentHealth -= value;
+        if(currentHealth <= 0)
+        {
+            GameObject.Destroy(this);
+        }
+    }
 }
--- Assets/Game/Classes/Player/PlayerWeapon1.cs
+++ Assets/Game/Classes/Player/PlayerWeapon1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerWeapon1 : MonoBehaviour {
     int Level = PlayerInventory.WeaponLevel;
     int BaseDamage = 10;
+    int damage = 0;
 
     void dealDamage()
     {
@@ -12,12 +13,20 @@ public class PlayerWeapon1 : MonoBehaviour {
         {
             for (int i = 0; i <= Level; i++)
             {
-                EnemyHealth.currentHealth -= BaseDamage;
+                damage += BaseDamage;
             }
         }
-        if (PlayerInventory.WeaponLevel == 10)
+        if (PlayerInventory.WeaponLevel >= 10)
         {
-            EnemyHealth.currentHealth -= BaseDamage *= 15;
+            damage += BaseDamage *= 15;
         }
+        if (PlayerInventory.WeaponLevel >= 15)
+        {
+            damage += BaseDamage *= 30;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent(EnemyHealth).TakeDamage(damage);
     }
 }
